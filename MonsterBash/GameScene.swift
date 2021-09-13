3b7756366eeea76221580655e4e9fad86e61e50d
sans-connaissance <davemalicke@gmail.com>
2021-09-13 13:11:15 -0400

    Rebuilding gameScene.sks file

--- MonsterBash/GameScene.swift
+++ MonsterBash/GameScene.swift
@@ -2,142 +2,93 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-
-  // Constants
-  let margin = CGFloat(30)
- 
-  // Buttons
-  var quirkButton: ButtonNode!
-  var zapButton: ButtonNode!
-  var munchButton: ButtonNode!
-
-  // Labels
-  let coin1Label = SKLabelNode(fontNamed: "Courier-Bold")
-  let coin2Label = SKLabelNode(fontNamed: "Courier-Bold")
-  
-  // Update time
-  var lastUpdateTimeInterval: TimeInterval = 0
-  
-  // Game over detection
-  var gameOver = false
-  
-  override func didMove(to view: SKView) {
-  
-    print("scene size: \(size)")
     
-    // Start background music
-    let bgMusic = SKAudioNode(fileNamed: "Latin_Industries.mp3")
-    bgMusic.autoplayLooped = true
-    addChild(bgMusic)
+    // Constants
+    let margin = CGFloat(30)
     
-    // Add background
-    let background = SKSpriteNode(imageNamed: "background")
-    background.position = CGPoint(x: size.width/2, y: size.height/2)
-    background.zPosition = -1
-    addChild(background)
-       
-    // Add quirk button
-    quirkButton = ButtonNode(iconName: "quirk1", text: "10", onButtonPress: quirkPressed)
-    quirkButton.position = CGPoint(x: size.width * 0.25, y: margin + quirkButton.size.height / 2)
-    addChild(quirkButton)
-
-    // Add zap button
-    zapButton = ButtonNode(iconName: "zap1", text: "25", onButtonPress: zapPressed)
-    zapButton.position = CGPoint(x: size.width * 0.5, y: margin + zapButton.size.height / 2)
-    addChild(zapButton)
+    // Update time
+    var lastUpdateTimeInterval: TimeInterval = 0
     
-    // Add munch button
-    munchButton = ButtonNode(iconName: "munch1", text: "50", onButtonPress: munchPressed)
-    munchButton.position = CGPoint(x: size.width * 0.75, y: margin + munchButton.size.height / 2)
-    addChild(munchButton)
+    // Game over detection
+    var gameOver = false
     
-    // Add coin 1 indicator
-    let coin1 = SKSpriteNode(imageNamed: "coin")
-    coin1.position = CGPoint(x: margin + coin1.size.width/2, y: size.height - margin - coin1.size.height/2)
-    addChild(coin1)
-    coin1Label.fontSize = 50
-    coin1Label.fontColor = SKColor.black
-    coin1Label.position = CGPoint(x: coin1.position.x + coin1.size.width/2 + margin, y: coin1.position.y)
-    coin1Label.zPosition = 1
-    coin1Label.horizontalAlignmentMode = .left
-    coin1Label.verticalAlignmentMode = .center
-    coin1Label.text = "10"
-    self.addChild(coin1Label)
+    // Graphical elements
+    var background: SKSpriteNode!
     
-    // Add coin 2 indicator
-    let coin2 = SKSpriteNode(imageNamed: "coin")
-    coin2.position = CGPoint(x: size.width - margin - coin1.size.width/2, y: size.height - margin - coin1.size.height/2)
-    addChild(coin2)
-    coin2Label.fontSize = 50
-    coin2Label.fontColor = SKColor.black
-    coin2Label.position = CGPoint(x: coin2.position.x - coin2.size.width/2 - margin, y: coin1.position.y)
-    coin2Label.zPosition = 1
-    coin2Label.horizontalAlignmentMode = .right
-    coin2Label.verticalAlignmentMode = .center
-    coin2Label.text = "10"
-    self.addChild(coin2Label)
-    
-  }
-  
-  func quirkPressed() {
-    print("Quirk pressed!")
-  }
-  
-  func zapPressed() {
-    print("Zap pressed!")
-  }
-  
-  func munchPressed() {
-    print("Munch pressed!")
-  }
-  
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let touch = touches.first else {
-      return
-    }
-    let touchLocation = touch.location(in: self)
-    print("\(touchLocation)")
-    
-    if gameOver {
-      let newScene = GameScene(size: size)
-      newScene.scaleMode = scaleMode
-      view?.presentScene(newScene, transition: SKTransition.flipHorizontal(withDuration: 0.5))
-      return
+    override func didMove(to view: SKView) {
+        
+        print("scene size: \(size)")
+        
+        // Start background music
+        let bgMusic = SKAudioNode(fileNamed: "Latin_Industries.mp3")
+        bgMusic.autoplayLooped = true
+        addChild(bgMusic)
+        
+        // bacground image
+        background = (self.childNode(withName: "background") as! SKSpriteNode)
+        
+        
     }
     
-  }
-  
-  func showRestartMenu(_ won: Bool) {
-    
-    if gameOver {
-      return;
-    }
-    gameOver = true
-    
-    let message = won ? "You win" : "You lose"
-    
-    let label = SKLabelNode(fontNamed: "Courier-Bold")
-    label.fontSize = 100
-    label.fontColor = SKColor.black
-    label.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
-    label.zPosition = 1
-    label.verticalAlignmentMode = .center
-    label.text = message
-    label.setScale(0)
-    addChild(label)
-    
-    let scaleAction = SKAction.scale(to: 1.0, duration: 0.5)
-    scaleAction.timingMode = SKActionTimingMode.easeInEaseOut
-    label.run(scaleAction)
-    
-  }
-  
- 
-  override func update(_ currentTime: TimeInterval) {
-    if gameOver {
-      return
+    func quirkPressed() {
+        print("Quirk pressed!")
     }
     
-  }
+    func zapPressed() {
+        print("Zap pressed!")
+    }
+    
+    func munchPressed() {
+        print("Munch pressed!")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        print("\(touchLocation)")
+        
+        if gameOver {
+            let newScene = GameScene(size: size)
+            newScene.scaleMode = scaleMode
+            view?.presentScene(newScene, transition: SKTransition.flipHorizontal(withDuration: 0.5))
+            return
+        }
+        
+    }
+    
+    func showRestartMenu(_ won: Bool) {
+        
+        if gameOver {
+            return;
+        }
+        gameOver = true
+        
+        let message = won ? "You win" : "You lose"
+        
+        let label = SKLabelNode(fontNamed: "Courier-Bold")
+        label.fontSize = 100
+        label.fontColor = SKColor.black
+        label.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        label.zPosition = 1
+        label.verticalAlignmentMode = .center
+        label.text = message
+        label.setScale(0)
+        addChild(label)
+        
+        let scaleAction = SKAction.scale(to: 1.0, duration: 0.5)
+        scaleAction.timingMode = SKActionTimingMode.easeInEaseOut
+        label.run(scaleAction)
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        if gameOver {
+            return
+        }
+        
+    }
 }
 
